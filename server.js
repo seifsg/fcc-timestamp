62cdc295ee3fff0d2909e23f61168f6ccbd13cf6
Seif Sgayer <seifsgayer@gmail.com>
2017-12-24 18:43:30 +0100

    server fix

--- server.js
+++ server.js
@@ -3,84 +3,90 @@
   * the verification process may break
   * ***************************************************/
 
-  'use strict';
+ var fs = require('fs');
+ var express = require('express');
+ var app = express();
 
-  var fs = require('fs');
-  var express = require('express');
-  var app = express();
- 
-  if (!process.env.DISABLE_XORIGIN) {
-    app.use(function (req, res, next) {
-      var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
-      var origin = req.headers.origin || '*';
-      if (!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1) {
-        console.log(origin);
-        res.setHeader('Access-Control-Allow-Origin', origin);
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      }
-      next();
-    });
-  }
- 
-  app.use('/public', express.static(process.cwd() + '/public'));
- 
-  app.route('/_api/package.json')
-    .get(function (req, res, next) {
-      console.log('requested');
-      fs.readFile(__dirname + '/package.json', function (err, data) {
-        if (err) return next(err);
-        res.type('txt').send(data.toString());
-      });
-    });
- 
-  app.route('/')
-    .get(function (req, res) {
-      res.sendFile(process.cwd() + '/views/index.html');
-    })
- 
-  // Respond not found to all the wrong routes
-  app.use(function (req, res, next) {
-    res.status(404);
-    res.type('txt').send('Not found');
-  });
- 
-  // Error Middleware
-  app.use(function (err, req, res, next) {
-    if (err) {
-      res.status(err.status || 500)
-        .type('txt')
-        .send(err.message || 'SERVER ERROR');
-    }
-  })
- 
-  app.listen(3000 || process.env.PORT, function () {
-    console.log('Node.js listening ...');
-  });
+ if (!process.env.DISABLE_XORIGIN) {
+   app.use(function (req, res, next) {
+     var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
+     var origin = req.headers.origin || '*';
+     if (!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1) {
+       console.log(origin);
+       res.setHeader('Access-Control-Allow-Origin', origin);
+       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+     }
+     next();
+   });
+ }
+
+ app.use('/public', express.static(process.cwd() + '/public'));
+
+ app.route('/_api/package.json')
+   .get(function (req, res, next) {
+     console.log('requested');
+     fs.readFile(__dirname + '/package.json', function (err, data) {
+       if (err) return next(err);
+       res.type('txt').send(data.toString());
+     });
+   });
+
+ app.route('/')
+   .get(function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+   })
 
 
-  // the actual function
+ // the actual function
 
-app.get("/api/timestamp/:datestring?", function (request, response) {
-  const ds = request.params.datestring;
-  let date;
+ app.get("/api/timestamp/:datestring?", function (request, response) {
+   const ds = request.params.datestring;
+   let date;
 
-  // if empty send back current date
-  if(ds === undefined){
-    date = new Date();
-    response.json({unix: date.getTime(), utc: date.toUTCString()}) ;
-  }else{
-    // see if its unix timestamp format  
-    const isUnix = ds.match(/^[0-9]+$/g) ? true : false;
-    // try to make a new date object with provided data
-    date = new Date(isUnix ? parseInt(ds) : ds);
+   // if empty send back current date
+   if (ds === undefined) {
+     date = new Date();
+     response.json({
+       unix: date.getTime(),
+       utc: date.toUTCString()
+     });
+   } else {
+     // see if its unix timestamp format  
+     const isUnix = ds.match(/^[0-9]+$/g) ? true : false;
+     // try to make a new date object with provided data
+     date = new Date(isUnix ? parseInt(ds) : ds);
 
-    // now test and send back response
-    const time = date.getTime();
-    if(isNaN(time)){
-      response.json({error: 'Invalid Date'});  
-    }else{
-      response.json({unix: time , utc: date.toUTCString()});
-    }
+     // now test and send back response
+     const time = date.getTime();
+     if (isNaN(time)) {
+       response.json({
+         error: 'Invalid Date'
+       });
+     } else {
+       response.json({
+         unix: time,
+         utc: date.toUTCString()
+       });
+     }
 
-  }
-});
+   }
+ });
+
+ // Respond not found to all the wrong routes
+ app.use(function (req, res, next) {
+   res.status(404);
+   res.type('txt').send('Not found');
+ });
+
+ // Error Middleware
+ app.use(function (err, req, res, next) {
+   if (err) {
+     res.status(err.status || 500)
+       .type('txt')
+       .send(err.message || 'SERVER ERROR');
+   }
+ })
+
+ app.listen(3000 || process.env.PORT, function () {
+   console.log('Node.js listening ...');
+ });
