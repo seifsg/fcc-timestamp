d89a0a5e858cb6acbcb7d8708d6cbf7459cea928
Glitch (cerulean-rifle) <none>
2017-12-24 14:57:52 +0000

    🌥👌 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,29 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+
+app.get("/api/timestamp/:datestring?", function (request, response) {
+  let result = {unix:'',utc:''};
+  response.send(result);
+});
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
